4bfc910b249c26038be82b557462cedf5759e33f
koki ito <k_ito@cm.is.ritsumei.ac.jp>
2019-07-16 15:37:46 +0900

    edit server.js Pages.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://todoapp1990359.herokuapp.com:4000")
+  res.header("Access-Control-Allow-Origin", "https://todoapp1990359.herokuapp.com:3000")
   //res.header("Access-Control-Allow-Origin", "http://127.0.0.1:4000")
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, HEAD, OPTIONS")
