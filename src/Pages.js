af3f600bad231961f3de608048a79f76e2387143
koki ito <k_ito@cm.is.ritsumei.ac.jp>
2019-07-16 15:27:37 +0900

    edit server.js Pages.js

--- src/Pages.js
+++ src/Pages.js
@@ -9,7 +9,7 @@ import immutable from 'immutable'
 import Api from './Api'
 
 //const api = new Api(`http://127.0.0.1:4000`)
-const api = new Api(`https://todoapp1990359.herokuapp.com`)
+const api = new Api(`https://todoapp1990359.herokuapp.com:4000`)
 
 const FavoriteButton = ({isDoing, onClick}) => (
   <FaRegCheckSquare style={{cursor: "pointer"}} color={isDoing ? "#ffa500" : "#eee"} onClick={onClick} />)
